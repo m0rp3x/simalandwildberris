0e54a7598a550546778fe8f8838bba3dbede7d28
m0rp3x <yurash612@gmail.com>
2025-04-03 22:40:46 +0300

    абоба 3(5)

---
+++ WBSL/Models/wildberries_parrent_category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WBSL.Models;
+
+public partial class wildberries_parrent_category
+{
+    public int id { get; set; }
+
+    public string name { get; set; } = null!;
+
+    public virtual ICollection<wildberries_category> wildberries_categories { get; set; } = new List<wildberries_category>();
+}
