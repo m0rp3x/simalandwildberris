2a763717ed6654811ff78bd89aba5bf2d1b8fb39
m0rp3x <yurash612@gmail.com>
2025-04-27 13:40:31 +0300

    DTO

---
+++ Shared/PriceCalculatorSettingsDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Shared;
+
+public class PriceCalculatorSettingsDto
+{
+    public decimal HandlingCost { get; set; } = 10m; // Стоимость обработки
+    public decimal PackagingCost { get; set; } = 10m; // Стоимость упаковки
+    public decimal SalaryCost { get; set; } = 10m; // Стоимость зарплаты
+    public decimal MarginPercent { get; set; } = 20m; // Процент маржи
+    public decimal RedemptionLossPercent { get; set; } = 3m; // Процент невыкупа
+    public decimal PlannedDiscountPercent { get; set; } = 60m; // Планируемая скидка
+}
---
+++ WBSL.Client/Pages/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+
+namespace WBSL.Client.Pages
+{
+    public static class Extensions
+    {
+        public static JsonElement? GetValueOrDefault(this Dictionary<string, JsonElement> dict, string key)
+        {
+            if (dict.TryGetValue(key, out var value))
+                return value;
+            return null;
+        }
+    }
+}
