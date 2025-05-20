905c1112e5baabc891c89c13dbf4ada2f5ac45bd
m0rp3x <yurash612@gmail.com>
2025-05-20 12:15:39 +0300

    423423442423

--- WBSL/Data/Models/BalanceUpdateRule.cs
+++ WBSL/Data/Models/BalanceUpdateRule.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WBSL.Data.Models;
 
@@ -8,6 +9,16 @@ public class BalanceUpdateRule
     public int Id{ get; set; } // Обязательно первичный ключ
     public int FromStock{ get; set; } // Сколько штук от
     public int ToStock{ get; set; } // Сколько штук до
+    [ScaffoldColumn(false)]
     public TimeSpan UpdateInterval{ get; set; } // Интервал обновления
     public DateTime CreatedAt{ get; set; } = DateTime.UtcNow; // Когда правило создано
+    
+    [NotMapped]
+    [Display(Name = "Интервал обновления (чч:мм:сс)")]
+    public string UpdateIntervalString
+    {
+        get => UpdateInterval.ToString(@"hh\:mm\:ss");
+        set => UpdateInterval = TimeSpan.TryParse(value, out var ts) ? ts : TimeSpan.Zero;
+    }
+
 }
